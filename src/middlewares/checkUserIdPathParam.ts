aa41ed7c90e0de51898d35463f19803694c8b380
Stepan Turchenko <stepanstepan4@gmail.com>
2023-11-07 17:18:15 +0200

    init balance operation logic + user.service base

--- src/middlewares/checkUserIdPathParam.ts
+++ src/middlewares/checkUserIdPathParam.ts
@@ -2,6 +2,6 @@ import { RequestHandler } from 'express';
 
 export const checkUserIdPathParam: RequestHandler<{ user_id: string }> = (req, res) => {
   const user_id = req.params.user_id;
-  if (!user_id) return res.status(400).json({ message: 'user_id query param is required' });
+  if (!user_id) return res.status(400).json({ message: 'user_id path param is required' });
   req.next();
 };
